be5bc9a48ae8be702362169b0696445bfd0dd48f
FalahFilano <falahnurli10@gmail.com>
2019-05-16 15:13:36 +0700

    add fragment class

---
+++ app/src/main/java/com/filano/sikemastekber/Fragment/ClassFragment.java
@@ -0,0 +1,31 @@
+package com.filano.sikemastekber.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.filano.sikemastekber.R;
+
+
+public class ClassFragment extends Fragment {
+
+    public ClassFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_class, container, false);
+        return rootView;
+    }
+}
